e2c9ef0b0d325bdb32dce50b898b534397c5d0c3
Kataev Denis <kataev@skydns.ru>
2014-01-09 16:18:03 +0600

    refactoring

--- django_union/union/tests.py
+++ django_union/union/tests.py
@@ -18,6 +18,8 @@ class TestModel(models.Model):
 class UnionTest(TestCase):
     def setUp(self):
         self.model = TestModel
+        self.connection = connections[TestModel.objects.db]
+
 
     def test_simple(self):
         queryset = self.model.objects.filter(text='filter').union(2013, 2014)
@@ -26,15 +28,14 @@ class UnionTest(TestCase):
 
     def dynamic_model(self, name='asd'):
         model = TestModel.objects.get_model(name, app_label='fake_app_label')
-        connection = connections[TestModel.objects.db]
-        cursor = connection.cursor()
+        cursor = self.connection.cursor()
 
         statements, pending = TestModel.objects.create_sql(model)
-        old_length = len(connection.introspection.table_names())
+        old_length = len(self.connection.introspection.table_names())
         for sql in statements:
             cursor.execute(sql, pending)
 
-        new_length = len(connection.introspection.table_names())
+        new_length = len(self.connection.introspection.table_names())
         self.assertEqual(old_length + 1, new_length)
         return model
 
@@ -46,8 +47,7 @@ class UnionTest(TestCase):
             m = model(text='filter')
             m.save()
         self.assertEqual(model.objects.count(), 10)
-        connection = connections[TestModel.objects.db]
-        self.assertTrue(name in connection.introspection.table_names())
+        self.assertTrue(name in self.connection.introspection.table_names())
         queryset = self.model.objects.filter(text='filter').union(name).all()
 
         query = queryset.fetch()
